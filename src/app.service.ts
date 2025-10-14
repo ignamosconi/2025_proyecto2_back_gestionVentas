bbe853fe979ca7f61a637d357df34447540fe6e8
Ignacio <ignamosconi@gmail.com>
2025-10-14 10:21:33 -0300

    Mensaje de WIP

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Estamos trabajando en su sistema de gestión de ventas :)';
   }
 }
